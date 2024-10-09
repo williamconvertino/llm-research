231a6c30c21cf2b6d245e37446e6573eb2c1c9d6
William Convertino <williamconvertino@gmail.com>
2024-10-09 19:50:42 -0400

    Quicksave

--- src/transformer/config/config_util.py
+++
@@ -1,5 +0,0 @@
-def first_not_none(*args):
-    for arg in args:
-        if arg is not None:
-            return arg
-    return None
---
+++ test.py
@@ -0,0 +1,4 @@
+from src.transformer import Transformer
+from src.transformer.config import TransformerConfig
+transformer_config = TransformerConfig()
+transformer = Transformer(transformer_config)
