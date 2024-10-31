0261791e3f62a84eb1b6b85daa81dca573874120
William Convertino <williamconvertino@gmail.com>
2024-10-31 14:59:55 -0400

    Fixed the requirements import

--- notebooks/notebook_setup.py
+++ notebooks/notebook_setup.py
@@ -15,3 +15,10 @@ except:
 path = os.path.abspath(os.path.join(os.getcwd(), '../'))
 if path not in sys.path:
   sys.path.append(path)
+  
+# Install the requirements
+try:
+  subprocess.check_call(["pip", "install", "-r", "../requirements.txt", "--quiet"])
+  print("Requirements installed")
+except:
+  print("Failed to install requirements")
