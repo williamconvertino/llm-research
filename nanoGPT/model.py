c1a3b2f0535656ca07f88aa9fff10b3a158924cc
William Convertino <williamconvertino@gmail.com>
2024-10-31 15:11:25 -0400

    Updated colab imports

--- nanoGPT/model.py
+++ nanoGPT/model.py
@@ -152,13 +152,12 @@ class GPTConfig:
     block_size: int = 1024
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12
-    n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
     use_ff: bool = True
     ln_setting: str = 'post' # 'pre' skip or 'post' 'skip' layer norm (or None)
-    input_vectors: tuple = ('w', 'w', 'w')
+    head_layout: list = [(12, 'w', 'w', 'w')]
 
 class GPT(nn.Module):
 
--- notebooks/notebook_setup.py
+++ notebooks/notebook_setup.py
@@ -1,15 +1,6 @@
 import sys
 import os
 import subprocess
-
-# Connect to colab if needed
-try:
-  from google.colab import drive
-  drive.mount('/content/drive')
-  os.chdir('/content/drive/My Drive/Research/llm-research')
-  print("Connected to google drive")
-except:
-  print("Not connected to google drive")
   
 # Set the sys path
 path = os.path.abspath(os.path.join(os.getcwd(), '../'))
