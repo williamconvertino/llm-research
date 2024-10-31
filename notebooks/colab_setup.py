acd4acc3616b3d7e822ff3b458108fe8ff4087a3
William Convertino <williamconvertino@gmail.com>
2024-10-31 14:42:42 -0400

    Fixed notebook imports

--- notebooks/colab_setup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-try:
-  from google.colab import drive
-  drive.mount('/content/drive')
-  os.chdir('/content/drive/My Drive/Research/llm-research')
-  print("Connected to google drive")
-except:
-  print("Not connected to google drive")
---
+++ notebooks/notebook_setup.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+# Connect to colab if needed
+try:
+  from google.colab import drive
+  drive.mount('/content/drive')
+  os.chdir('/content/drive/My Drive/Research/llm-research')
+  print("Connected to google drive")
+except:
+  print("Not connected to google drive")
+  
+# Set the sys path
+path = os.path.abspath(os.path.join(os.getcwd(), '../'))
+if path not in sys.path:
+  sys.path.append(path)
