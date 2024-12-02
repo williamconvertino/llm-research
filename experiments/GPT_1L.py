574492f2cc579b17caf371beb3711fb3218ab743
William Convertino <williamconvertino@gmail.com>
2024-12-02 16:24:19 -0500

    Updated experiments

--- experiments/GPT_1L.py
+++ experiments/GPT_1L.py
@@ -1,5 +1,5 @@
 import experiment_base
-from experiment_base import train_GPT, train_GDM, GPT_CONFIG, GDM_CONFIG
+from experiment_base import run_experiment, GPT_CONFIG, GDM_CONFIG
 
 config = GPT_CONFIG
-train_GPT(config)
+run_experiment(config)
