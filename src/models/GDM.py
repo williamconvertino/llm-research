48a17137adfc698870f2f935f1a8874785e94cc7
William Convertino <williamconvertino@gmail.com>
2024-12-02 00:09:06 -0500

    Debugging

--- src/models/GDM.py
+++ src/models/GDM.py
@@ -124,7 +124,7 @@ class GDM(nn.Module):
     p = self.W_p(torch.arange(S + 1, device=device))
     
     Q = p @ self.W_q
-    K = p @ self.W_k
+    K = p[:, :-1] @ self.W_k
     
     if self.attn_kernel_fn == 'softmax':
       attn_scores = F.softmax(torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.d_embed), dim=-1)
