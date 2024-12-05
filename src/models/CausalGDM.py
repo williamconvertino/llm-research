d936f1375c4751eebfac3e72ed2962994ca3534b
William Convertino <williamconvertino@gmail.com>
2024-12-05 00:56:07 -0500

    Fixed typo

--- src/models/CausalGDM.py
+++ src/models/CausalGDM.py
@@ -101,7 +101,7 @@ class CausalGDM(nn.Module):
     
     f_k = torch.zeros_like(e, device=device)
 
-    for _ in self.config.n_layer:
+    for _ in range(self.config.n_layer):
       f_k = f_k + self.gd_step(f_k, e, krn)
       if self.config.use_ff:
         f_k = f_k + self.mlp(self.ln_mlp(f_k))
