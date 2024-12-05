Fixed attention mask

src/models/CausalGDM.py
@@ -45,7 +45,7 @@ class GDAttention(nn.Module):
     
     # y = torch.nn.functional.scaled_dot_product_attention(Q, K, V, attn_mask=mask, dropout_p=self.dropout if self.training else 0)
     attn_scores = Q @ K.transpose(-2, -1) / math.sqrt(self.d_embed)
-    attn_scores = attn_scores.masked_fill(mask, -1e9)
+    attn_scores = attn_scores.masked_fill(mask.logical_not(), float('-inf'))
     attn_scores = F.softmax(attn_scores, dim=-1)
     attn_scores = self.attn_dropout(attn_scores)
     y = attn_scores @ V