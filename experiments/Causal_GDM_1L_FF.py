8ca1919b700efec0d62d15ff365518003417b38f
William Convertino <williamconvertino@gmail.com>
2024-12-04 19:57:20 -0500

    Added FF experiment

--- experiments/Causal_GDM_1L_FF.py
+++ experiments/Causal_GDM_1L_FF.py
@@ -2,5 +2,5 @@ import experiment_base
 from experiment_base import run_experiment, GPT_CONFIG, GDM_CONFIG, CAUSAL_GDM_CONFIG
 
 config = CAUSAL_GDM_CONFIG
-config.use_ff = False
+config.use_ff = True
 run_experiment(config)
