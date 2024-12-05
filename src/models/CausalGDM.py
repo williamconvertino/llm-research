e7c8904635c9100b231bfa95f37265790a834187
William Convertino <williamconvertino@gmail.com>
2024-12-05 01:05:04 -0500

    Debugging

--- src/models/CausalGDM.py
+++ src/models/CausalGDM.py
@@ -62,9 +62,7 @@ class CausalGDM(nn.Module):
   
   def gd_step(self, f_k, e, krn):
     R = torch.softmax(self.wte.weight @ f_k.transpose(1, 2), dim=-1)
-    print(R.shape)
-    print(self.wte.weight.shape)
-    ex_wte = R @ self.wte.weight
+    ex_wte = R.transpose(-1, -2) @ self.wte.weight.transpose(-1, -2)
     ex_wte = ex_wte.sum(dim=1) / R.sum(dim=1)
 
     V = e - ex_wte
